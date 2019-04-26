493ace748b2d67c4ec3cc03c294f5f2aac308954
aliosman <aliosmanoktar@gmail.com>
2019-04-27 00:15:59 +0300

    Ürün Arama Eklendi

--- E-Mall_Api/E-Mall_Api/Controllers/UrunlerController.cs
+++ E-Mall_Api/E-Mall_Api/Controllers/UrunlerController.cs
@@ -8,7 +8,46 @@ namespace E_Mall_Api.Controllers
 {
     public class UrunlerController : ApiController
     {
-        public List<Urun> Get(int KategoriID)
+        public List<Urun> Get(string aranan=null,int KategoriID = -1)
+        {
+            if (aranan != null)
+                return Arama(aranan);
+            else if (KategoriID != -1)
+                return Get(KategoriID);
+            else
+                return new List<Urun>();
+        }
+        private List<Urun> Arama(string aranan)
+        {
+            string sorgu = $"select * from Urun where Adi like '%{aranan}%'";
+            Debug.WriteLine(sorgu);
+            List<Urun> items = new List<Urun>();
+            SqlDataReader rd = Database.Database.GetReader(sorgu);
+            while (rd.Read())
+                items.Add(new Urun()
+                {
+                    Adi = rd["Adi"].ToString(),
+                    EskiFiyat = rd["EskiFiyat"].ToString().parse<float>(),
+                    Fiyat = rd["Fiyat"].ToString().parse<float>(),
+                    ID = (int)rd["ID"]
+                });
+
+            rd.Close();
+            foreach (Urun item in items)
+            {
+                List<string> images = new List<string>();
+                sorgu = string.Format("select * from Resim where UrunID={0}", item.ID);
+                rd = Database.Database.GetReader(sorgu);
+                while (rd.Read())
+                {
+                    images.Add(HomeController.IP + rd["Yol"].ToString());
+                }
+                rd.Close();
+                item.Resimler = images;
+            }
+            return items;
+        }
+        private List<Urun> Get(int KategoriID)
         {
             List<Urun> items = new List<Urun>();
             string sorgu = "with ct(ID) as (" +
